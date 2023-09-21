02a97bd1b87c83d1dafcb95a3d8eff6b0fb3eb1c
hammadx02 <hammadaali02@gmail.com>
2023-09-22 00:56:25 +0500

    updated

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-const myBorderColor = Color(0xffE8ECF4);
-const myDarkGreyColor = Color(0xff6A707C);
-const myGreyColor = Color(0xff8391A1);
-const myPrimaryColor = Color(0xff35C2C1);
-const myBlackColor = Color(0xFF1E232C);
+const myBorderColor = Color(0XFFF7F8F9);
+const myDarkGreyColor = Color(0XFF6A707C);
+const myGreyColor = Color(0XFF8391A1);
+const myPrimaryColor = Color(0XFF35C2C1);
+const myBlackColor = Color(0XFF1E232C);
